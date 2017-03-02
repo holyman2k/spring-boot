53cc38aaa3d9d4243ada2d6eb69fc370ce29c686
Charlie Wu <c.wu@webfm.net>
2017-03-02 13:59:07 +1100

    Add logging for user detail service

--- src/main/java/com/charliewu/common/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/charliewu/common/services/impl/UserDetailsServiceImpl.java
@@ -2,6 +2,9 @@ package com.charliewu.common.services.impl;
 
 import com.charliewu.common.domain.Account;
 import com.charliewu.common.repositories.AccountRepository;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +17,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+
     @Autowired
     private AccountRepository accountRepository;
 
@@ -25,9 +31,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 //            Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
 //            grantedAuthorities.add(new SimpleGrantedAuthority("User"));
 //            return new User(account.getUsername(), account.getPassword(), grantedAuthorities);
+            logger.debug("user logged in: {}", account.getName());
             return new UserAccount(account);
         }
-
+        logger.debug("username: {} not found", username);
         throw new UsernameNotFoundException("user name not found");
     }
 }
